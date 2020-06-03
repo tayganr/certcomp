15b13b03b36435794c5acdd47d08768bf634f399
Taygan Rifat <taygan@Taygans-MacBook-Pro.local>
2020-06-03 22:01:21 +0100

    comp exam

--- competencies/__init__.py
+++ competencies/__init__.py
@@ -2,6 +2,7 @@ import os
 import logging
 import requests
 import json
+import math
 from datetime import datetime
 import pandas as pd
 from lxml import html
@@ -17,15 +18,17 @@ CONTAINER_NAME = os.environ.get('CERT_COMP_STORAGE_CONTAINER')
 FILE_NAME_COMPS = 'competencies'
 FILE_NAME_COMPEXAMS = 'comp_exams'
 COLUMNS_COMPS = ['AREA', 'COMPETENCY', 'LINK']
-COLUMNS_COMPEXAMS = ['COMPETENCY','OPTION','LEVEL','EXAM']
+COLUMNS_COMPEXAMS = ['COMPETENCY', 'EXAM']
 
 # Microsoft Competencies
 URL = 'https://partner.microsoft.com/en-us/membership/competencies'
 XPATH_COMPETENCY_AREAS = '//div[@class="panel-group hidden-md-x hidden-lg simple-tabs-accordion-sm"]/div'
 XPATH_COMPETENCIES = './/div[@class="col-xs-12 col-sm-6 col-md-x-4 clickable-panel column-content-item"]'
-# COMPETENCY_URL_PREFIX = 'https://partner.microsoft.com'
-XPATH_COMPETENCY_OPTIONS = '//div[@class="complex-table-accordion-device"]//div[@class="panel panel-default"]'
-XPATH_COMPETENCY_TIERS = './/div[@class="panel-body"]//div[@class="col-md-12"]'
+
+# CERT API
+CERT_API = 'https://docs.microsoft.com/api/contentbrowser/search/certifications'
+CERT_BATCH = 30
+MATCH = 'microsoft.com/en-us/learn'
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
@@ -34,6 +37,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     data_compexams = []
     response = {}
     response['competencies'] = {}
+    cert_dict = get_cert_dict()
 
     # 2. HTTP Request
     page = requests.get(URL)
@@ -57,20 +61,36 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     for area in response['competencies']:
         for competency in response['competencies'][area]:
             logging.info('Competency: ' + competency['competency'])
-            page_comp = requests.get(competency['link'])
-            tree_comp = html.fromstring(page_comp.content)
+            comp_page = requests.get(competency['link'])
+            comp_tree = html.fromstring(comp_page.content)
+            
+            link_dict = {}
+            for links in comp_tree.xpath('.//a'):
+                if 'href' in links.attrib:
+                    link = links.attrib["href"]
+                    if MATCH in link and 'exam-70-' not in link:
+                        link_dict[link] = None
+            
+            uid_dict = {}
+            for filtered_link in link_dict:
+                link_response = requests.get(filtered_link)
+                link_tree = html.fromstring(link_response.content)
+                meta = link_tree.xpath('//meta[@name="uid"]')
+                if len(meta) > 0:
+                    if 'content' in meta[0].attrib:
+                        uid = meta[0].attrib["content"]
+                        uid_dict[uid] = None
 
-            for section in tree_comp.xpath(XPATH_COMPETENCY_OPTIONS):
-                option = section.xpath('.//span[@class="accordion-heading-text"]/text()')[0]
-                logging.info('Option: ' + option)
-               
-                toggle = {'Gold':'Silver','Silver':'Gold'}
-                level = 'Gold'
-                for segment in section.xpath(XPATH_COMPETENCY_TIERS):
-                    level = toggle[level]
-                    logging.info('-- ' + level)
-                    for links in segment.xpath('.//a'):
-                        data_compexams = populate_table(competency['competency'], option, level, links, data_compexams)
+            for filtered_uid in uid_dict:
+                if filtered_uid[:4] == 'exam':
+                    exam_id = filtered_uid.replace('exam.','')
+                    compexam_row = [competency['competency'], exam_id.upper()]
+                    data_compexams.append(compexam_row)
+                
+                if filtered_uid[:13] == 'certification':
+                    for exam in cert_dict[filtered_uid]:
+                        compexam_row = [competency['competency'], exam.upper()]
+                        data_compexams.append(compexam_row)
 
     # 4. Write Data to Blob Storage
     block_blob_service = BlockBlobService(account_name=ACCOUNT_NAME, account_key=ACCOUNT_KEY)     
@@ -80,6 +100,46 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # 5. HTTP Response
     return func.HttpResponse(json.dumps(response),headers={'Content-Type':'application/json'})
 
+def get_cert_dict():
+    # Initialise Dictionary
+    cert_dict = {}
+
+    # Calculate number of pages
+    number_of_items = get_cert_data(0)['count']
+    number_of_pages = math.ceil(number_of_items/CERT_BATCH)
+
+    # Get Certification Data
+    for page in range(0,number_of_pages):
+        skip = page * 30
+        data = get_cert_data(skip)
+        for result in data['results']:
+            cert_id = result['uid']
+            cert_dict[cert_id] = []
+            for exam in result['exams']:
+                exam_id = exam['display_name']
+                cert_dict[cert_id].append(exam_id)
+    
+    # Return Dictionary
+    return cert_dict
+
+def get_cert_data(skip):
+    params = {
+        'environment':'prod',
+        'locale':'en-us',
+        'facet':'levels',
+        'facet':'products',
+        'facet':'resource_type',
+        'facet':'roles',
+        'facet':'type',
+        '$filter':"((resource_type eq 'certification'))",
+        '$orderBy':"last_modified desc",
+        '$skip':skip,
+        '%$top':CERT_BATCH
+    }
+    response = requests.get(CERT_API, params=params)
+    data = json.loads(response.content)
+    return data
+
 def write_to_blob(block_blob_service, data, columns, filename):
     today = datetime.today()
     folderpath = '{0}/{1}'.format(filename, today.strftime('%Y/%m/%d'))
@@ -87,60 +147,3 @@ def write_to_blob(block_blob_service, data, columns, filename):
     df = pd.DataFrame(data, columns=columns)
     csv = df.to_csv(index=False, encoding='utf-8')
     block_blob_service.create_blob_from_text(container_name=CONTAINER_NAME, blob_name=filename, text=csv, encoding='utf-8')
-
-def populate_table(competency_name, option, level, links, table):
-    if 'href' in links.attrib:
-        link = links.attrib["href"] 
-        link_title = extract(links.xpath('./text()'))
-        prefix = 'https://www.microsoft.com/en-us/learning/exam-'
-
-        if link_title is None:
-            link_title = extract(links.xpath('./span/text()'))
-
-        if link_title is not None:
-            if prefix in link or link_title.startswith('Exam'):
-                exam_id = get_exam_id(link_title)
-                row = (competency_name, option, level, exam_id)
-                table.append(row)
-            elif 'https://www.microsoft.com/en-us/learning/' in link:
-                link_response = requests.get(link)
-                link_tree = html.fromstring(link_response.content)
-
-                for exam in link_tree.xpath('//*[@id="msl-certification-azure"]/div[1]/section/div/div/p[1]/a'):
-                    exam_id = exam.xpath('./text()')[0].replace('Exam ','').replace('Transition ','')
-                    row = (competency_name, option, level, exam_id)
-                    table.append(row)
-
-    return table
-
-def extract(data):
-    if data:
-        return data[0].strip()
-    else:
-        return None
-
-def get_link(href):
-    if href is not None:
-        return href.attrib['href']
-    else:
-        return None
-
-def get_exam_id(text):
-    exam_id = None
-
-    if text[:5] == 'Exam ':
-        exam_id = text.replace('Exam ', '')
-
-    if exam_id is None:
-        exam_id = text
-    
-    if ':' in exam_id:
-        exam_id = exam_id.split(':')[0]
-
-    if exam_id == '532':
-        exam_id = '70-532'
-
-    if len(exam_id) > 7:
-        exam_id = exam_id.split(' ')[0]
-
-    return exam_id
