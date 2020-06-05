8ef10b40aa568f83aa9882e940105a172cc85e9e
Taygan Rifat <taygan@Taygans-MacBook-Pro.local>
2020-06-05 22:51:33 +0100

    retired exams

--- retired/__init__.py
+++ retired/__init__.py
@@ -32,31 +32,41 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     page = requests.get(URL)
     tree = html.fromstring(page.content)
 
-    # 3. Get Data
+    # 3. Exams Scheduled to Retire
     for section in tree.xpath('//*[@id="content"]/div/div/div/div[3]/div/table'):
         retirement_date_raw = section.xpath('.//tbody/tr/td[1]/text()')
-        logging.info('RETIRE: ' + retirement_date_raw[0])
+        # logging.info('RETIRE: ' + retirement_date_raw[0])
 
         if retirement_date_raw:
             retirement_date_raw = retirement_date_raw[0].strip()
             retirement_date_raw = retirement_date_raw.replace('Retiring on ', '')
-
-            retirement_month = None
-            retirement_date = None
-            for month in MONTHS:
-                if month in retirement_date_raw.upper():
-                    retirement_month = month
-                    retirement_year = retirement_date_raw[-4:]
-                    retirement_day = retirement_date_raw.upper().replace(retirement_month, '').replace(retirement_year, '').replace(',', '').replace(' ', '')
-                    retirement_date_text = '{0} {1} {2}'.format(retirement_day, retirement_month, retirement_year)
-                    retirement_date = datetime.strptime(retirement_date_text, '%d %B %Y')
+            retirement_date = get_retirement_date(retirement_date_raw)
             
             for exam in section.xpath('.//tbody/tr/td[2]/p/a'):
                 exam_id = exam.xpath('./text()')[0].strip().replace(':', '')
                 row = [exam_id, retirement_date]
                 response[exam_id] = str(retirement_date)
-                logging.info(exam_id)
                 data.append(row)
+    
+    # Exams Recently Retired (othercert-1)
+    for trow in tree.xpath('//*[@id="othercert-1"]/table/tbody/tr'):
+        cells = trow.xpath('.//td')
+        exam_id = cells[0].xpath('.//text()')[0]
+        retire_date = cells[2].xpath('.//text()')[0]
+        retire_date = get_retirement_date(retire_date)
+        row = [exam_id, retire_date]
+        response[exam_id] = str(retire_date)
+        data.append(row)
+
+    # Exams Recently Retired (othercert-2)
+    for trow in tree.xpath('//*[@id="othercert-2"]/table/tbody/tr'):
+        cells = trow.xpath('.//td')
+        exam_id = cells[0].xpath('.//text()')[0]
+        retire_date = cells[2].xpath('.//text()')[0]
+        retire_date = get_retirement_date(retire_date)
+        row = [exam_id, retire_date]
+        response[exam_id] = str(retire_date)
+        data.append(row)
       
     # 4. Write to Azure Blob Storage
     block_blob_service = BlockBlobService(account_name=ACCOUNT_NAME, account_key=ACCOUNT_KEY)     
@@ -64,6 +74,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # 5. HTTP Response
     return func.HttpResponse(json.dumps(response),headers={'Content-Type':'application/json'})
+
+def get_retirement_date(retirement_date_raw):
+    retirement_date = None
+    for month in MONTHS:
+        if month in retirement_date_raw.upper():
+            retirement_month = month
+            retirement_year = retirement_date_raw[-4:]
+            retirement_day = retirement_date_raw.upper().replace(retirement_month, '').replace(retirement_year, '').replace(',', '').replace(' ', '')
+            retirement_date_text = '{0} {1} {2}'.format(retirement_day, retirement_month, retirement_year)
+            retirement_date = datetime.strptime(retirement_date_text, '%d %B %Y')
+    return retirement_date
 
 def write_to_blob(block_blob_service, data, columns, filename):
     today = datetime.today()
